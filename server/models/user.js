d6f3e0a5ea5730d911aa6536f5ed92db9c6e4904
Stefano Corra <stefano.corra@hotmail.com>
2019-01-04 18:50:25 -0500

    token passw

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require ('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
+
 
 
 const userSchema = mongoose.Schema({
@@ -62,7 +65,25 @@ userSchema.pre('save',function(next){
 
 })
 
+userSchema.methods.comparePassword = function(candidatePassword,cb){
+  bcrypt.compare(candidatePassword, this.password, function(err,isMatch){
+    if(err) return cb(err);
+    cb(null,isMatch)
+  })
+}
 
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //token = user.id + password
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET)
+
+    user.token = token;
+    user.save(function(err,user){
+      if(err) return cb(err);
+      cb(null,user);
+    })
+}
 
 
 const User = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -16,16 +16,17 @@ app.use(cookieParser());
 
 // Models
 
-const {User} = require('./models/user');
+const { User } = require('./models/user');
 
 
 //================================
 //       USERS
 //================================
 
+// user register
 app.post('/api/users/register',(req,res)=>{
        //res.status(200);
-       const user = new User(req.body);
+        const user = new User(req.body);
 
        user.save((err,doc)=>{
 
@@ -38,7 +39,28 @@ app.post('/api/users/register',(req,res)=>{
            userdata: doc
          })
        })
+});
+
+// user login
+app.post('/api/users/login',(req,res)=>{
+  //find the email
+  User.findOne({'email':req.body.email},(err,user)=>{
+     if(!user) return res.json({loginSuccess:false, message: 'Auth failed, email not found'});
+     //check the password
+     user.comparePassword(req.body.password,(err,isMatch)=>{
+        if(!isMatch) return res.json({loginSuccess:false, message:'Wrong password'});
+        // if email/password correct => generate token
+        user.generateToken((err,user)=>{
+            if(err) return res.status(400).send(err);
+            res.cookie('w_auth',user.token).status(200).json({loginSuccess:true});
+        })
+     })
+  })
+
+
+
 })
+
 
 const port = process.env.PORT || 3002;
 
