8b3a0515751b560f122c7a5eb45b2f029c54fa8c
Stefano Corra <stefano.corra@hotmail.com>
2019-01-12 16:37:04 -0500

    home slider

---
+++ client/src/components/Home/home_slider.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import MyButton from '../utils/button';
+
+const HomeSlider = (props) => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  }
+
+  const slides=[
+    {
+      img:'/images/featured/featured_home.jpg',
+      lineOne:'Fender',
+      lineTwo:'Custom shop',
+      linkTitle:'Shop Now',
+      linkTo:'/shop',
+    },
+    {
+      img:'/images/featured/featured_home_2.jpg',
+      lineOne:'B-Stock',
+      lineTwo:'Awesome Discounts',
+      linkTitle:'View Offers',
+      linkTo:'/shop',
+    }
+  ]
+
+
+
+function generateSlides () {
+  return(
+    slides?
+        slides.map((item,i)=>(
+
+          <div key={i}>
+            <div className="featured_image"
+            style={{
+              background:`url(${item.img})`,
+              height:`${window.innerHeight}px`
+            }}
+            >
+            <div className="featured_action">
+              <div className="tag title">{item.lineOne}</div>
+              <div className="tag low_title">{item.lineTwo}</div>
+              <div>
+                <MyButton
+                  type="default"
+                  title={item.linkTitle}
+                  linkTo={item.linkTo}
+                  addStyles={{
+                    margin: '10px 0 0 0'
+                  }}
+                />
+              </div>
+            </div>
+            </div>
+          </div>
+        ))
+    :null
+  )}
+
+  return(
+
+    <div className="featured_container">
+      <Slider {...settings}>
+        {generateSlides()}
+      </Slider>
+    </div>
+)};
+
+export default HomeSlider;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import HomeSlider from './home_slider';
+import HomePromotion from './home_promotion';
 
 class Home extends Component {
 
   render() {
     return (
       <div>
-        Home
+        <HomeSlider />
+        <HomePromotion />
       </div>
     );
   }
