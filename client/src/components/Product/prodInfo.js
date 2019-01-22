e22feaa1bbc5cc28ede2e932f9ff2722c6f659cf
Stefano Corra <stefano.corra@hotmail.com>
2019-01-21 20:24:24 -0500

    client product info detail

--- client/src/components/Product/prodInfo.js
+++ client/src/components/Product/prodInfo.js
@@ -8,6 +8,80 @@ import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 
 const ProdInfo = (props) => {
 
+    const showProdTags =(detail) => (
+
+            <div className="product_tags">
+              {
+                detail.shipping ?
+                <div className="tag">
+                  <div>
+                    <FontAwesomeIcon icon={faTruck}/>
+                  </div>
+                  <div className="tag_text">
+                      <div>Free Shipping</div>
+                      <div>And return</div>
+                  </div>
+                </div>
+
+                :null
+              }
+              {
+                detail.available ?
+
+                    <div className="tag">
+                      <div>
+                        <FontAwesomeIcon icon={faCheck}/>
+                      </div>
+                      <div className="tag_text">
+                          <div>Available</div>
+                          <div>in Store</div>
+                      </div>
+                    </div>
+
+                :
+
+                <div className="tag">
+                  <div>
+                    <FontAwesomeIcon icon={faTimes}/>
+                  </div>
+                  <div className="tag_text">
+                      <div>Not Available</div>
+                      <div>Preorder Only</div>
+                  </div>
+                </div>
+
+              }
+            </div>
+      )
+
+    const showProdActions = (detail) =>(
+
+          <div className="product_actions">
+            <div className="price">$ { detail.price }</div>
+            <div className="cart">
+              <MyButton
+                  type="add_to_cart_link"
+                  runAction = {()=>{
+                    console.log('add to cart');
+                  }}
+                />
+            </div>
+          </div>
+
+    )
+
+    const showProSpecifications =() =>(
+      <div className="product_specifications">
+          <h2>Specs</h2>
+            <div className="item">
+              <strong>Frets: </strong> {detail.frets}
+            </div>
+            <div className="item">
+              <strong>Wood: </strong> {detail.wood.name}
+            </div>
+      </div>
+    )
+
     const detail = props.detail;
 
   return(
@@ -16,6 +90,9 @@ const ProdInfo = (props) => {
       <p>
         {detail.description}
       </p>
+      { showProdTags(detail) }
+      { showProdActions(detail) }
+      { showProSpecifications(detail) }
   </div>
 )};
 
