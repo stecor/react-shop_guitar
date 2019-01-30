b78933d8526afac1caba419c66d5c91179275700
Stefano Corra <stefano.corra@hotmail.com>
2019-01-30 00:03:14 -0500

    change misc

--- client/src/components/utils/misc.js
+++ client/src/components/utils/misc.js
@@ -1,4 +1,5 @@
 // SERVER ROUTES
 
 export const USER_SERVER = '/api/users';
-export const PRODUCT_SERVER = '/api/product/'
+export const PRODUCT_SERVER = '/api/product';
+export const SITE_SERVER = '/api/site'
